bdf6fd04763ad687d0b526f871e67b064461be64
Will Monroe <wmonroe4@stanford.edu>
2016-08-19 22:00:13 -0700

    Separate headwords and forms in validation

--- dictionary/validate.py
+++ dictionary/validate.py
@@ -13,6 +13,7 @@ def find_bad_entries():
     from .alutiiq import get_endings_map, is_valid
 
     entries = group_entries(Chunk.objects.all(), separate_roots=True)
+    bad_headwords = []
     bad_forms = []
     for i, entry in enumerate(entries):
         if i % 100 == 0:
@@ -20,15 +21,18 @@ def find_bad_entries():
         for root in entry.roots:
             word = root.word.replace('(', '').replace(')', '')
             if not is_valid(word):
-                bad_forms.append(('-', word, '-'))
+                bad_headwords.append(word)
             elif root.pos and root.pos != 'None':
                 for tags, form in get_endings_map(root.root, root.pos).iteritems():
                     form = form.replace('(', '').replace(')', '')
                     if form != '-' and not is_valid(form):
                         bad_forms.append((form, root.word, tags))
-        if len(bad_forms) > 200:
+        if len(bad_forms) > 200 or len(bad_headwords) > 200:
             break
 
+    sample_size = min(100, len(bad_headwords))
+    for word in random.sample(bad_headwords, sample_size):
+        print(u'{0: <25}'.format(word).encode('utf-8'))
     sample_size = min(100, len(bad_forms))
     for form, word, tags in random.sample(bad_forms, sample_size):
         print(u'{0: <51} {1}'.format(u'{0: <25} {1}'.format(form, word), tags).encode('utf-8'))
