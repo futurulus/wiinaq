e42d72244f9cfee2ceffa486e8b91b6b61f63d63
Will Monroe <wmonroe4@stanford.edu>
2018-06-29 14:55:16 -0800

    Add models and fields for combined dictionary

--- dictionary/admin.py
+++ dictionary/admin.py
@@ -1,19 +1,92 @@
 from django.contrib import admin
 
-from .models import Entry, Source
+from .models import Entry, Source, Example, Variety
+from .models import SeeAlso, EntryExampleInfo, EntryVarietyInfo, ExampleVarietyInfo
 
 
-class EntryAdmin(admin.ModelAdmin):
+class CustomTitleAdmin(admin.ModelAdmin):
+    custom_title = None
+
+    def changelist_view(self, request, extra_context=None):
+        if self.custom_title is not None:
+            extra_context = {'title': self.custom_title}
+        return super(CustomTitleAdmin, self).changelist_view(request, extra_context=extra_context)
+
+
+class EntryExamplesInline(admin.TabularInline):
+    model = EntryExampleInfo
+    raw_id_fields = ['example']
+    extra = 1  # how many rows to show
+    verbose_name = 'example'
+    verbose_name_plural = 'examples'
+
+
+class ExampleEntriesInline(admin.TabularInline):
+    model = EntryExampleInfo
+    raw_id_fields = ['entry']
+    extra = 1  # how many rows to show
+    verbose_name = 'entry'
+    verbose_name_plural = 'entries'
+
+
+class EntryVarietyInfoInline(admin.TabularInline):
+    model = EntryVarietyInfo
+    extra = 1  # how many rows to show
+    verbose_name = 'variety'
+    verbose_name_plural = 'language variety information'
+
+
+class ExampleVarietyInfoInline(admin.TabularInline):
+    model = ExampleVarietyInfo
+    extra = 1  # how many rows to show
+    verbose_name = 'variety'
+    verbose_name_plural = 'language variety information'
+
+
+class SeeAlsoInline(admin.TabularInline):
+    model = SeeAlso
+    raw_id_fields = ['target']
+    fk_name = 'source'
+    extra = 1  # how many rows to show
+    verbose_name = 'See Also link'
+    verbose_name_plural = 'See Also links'
+
+
+class EntryAdmin(CustomTitleAdmin):
+    custom_title = 'An entry is a single definition of a single word from a single source. ' \
+                   'All entries for the same word are shown on that word\'s page.'
     readonly_fields = ('pos_auto', 'root_auto', 'search_text')
     list_display = ['entry', 'pos_final', 'defn']
-    list_filter = ['pos']
+    list_filter = ['pos', 'source']
     search_fields = ['entry', 'defn']
+    filter_horizontal = ['examples']
+    inlines = (EntryVarietyInfoInline, EntryExamplesInline, SeeAlsoInline)
     save_as = True
 
 admin.site.register(Entry, EntryAdmin)
 
 
-class SourceAdmin(admin.ModelAdmin):
+class SourceAdmin(CustomTitleAdmin):
+    custom_title = 'Sources are where we heard the word from: ' \
+                   'speakers, books, other dictionaries, etc.'
     list_display = ['abbrev', 'description']
 
 admin.site.register(Source, SourceAdmin)
+
+
+class VarietyAdmin(CustomTitleAdmin):
+    custom_title = 'A short list of broad language varieties, dialects, or geographic regions. ' \
+                   'For smaller regions, cities, or speakers, use the "Details" field.'
+    list_display = ['abbrev', 'description']
+
+admin.site.register(Variety, VarietyAdmin)
+
+
+class ExampleAdmin(CustomTitleAdmin):
+    custom_title = "Example sentences and phrases showing word usage. Make sure they're added " \
+                   "to an Entry or people can't see them."
+    list_display = ['vernacular', 'english']
+    search_fields = ['vernacular', 'english']
+    inlines = (ExampleVarietyInfoInline, ExampleEntriesInline)
+
+admin.site.register(Example, ExampleAdmin)
--- dictionary/models.py
+++ dictionary/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import truncatechars
 
 from .alutiiq import normalize, get_pos, get_root
 
@@ -6,6 +7,12 @@ from .alutiiq import normalize, get_pos, get_root
 class Source(models.Model):
     abbrev = models.CharField('abbreviation', unique=True, max_length=100)
     description = models.CharField('description', max_length=1000)
+    ordering = models.IntegerField('sort location',
+                                   default=0,
+                                   help_text='Controls what order sources show up in each '
+                                             'dictionary entry. Sources with lower values of this '
+                                             'number will appear first, with ties broken by '
+                                             'alphabetical ordering of the abbreviation.')
 
     def annotated(self):
         return '<span class="source" title="%s">%s</span>' % (self.description, self.abbrev)
@@ -14,19 +21,59 @@ class Source(models.Model):
         return self.description
 
 
+class Variety(models.Model):
+    abbrev = models.CharField('abbreviation', unique=True, max_length=100)
+    description = models.CharField(max_length=1000)
+
+    class Meta:
+        verbose_name_plural = "varieties"
+
+    def annotated(self):
+        return '<span class="variety" title="%s">%s</span>' % (self.description, self.abbrev)
+
+    def __unicode__(self):
+        return self.description
+
+
+class Example(models.Model):
+    vernacular = models.TextField('Alutiiq',
+                                  help_text='Text in Alutiiq (or perhaps other focus languages '
+                                            'in the future?)')
+    english = models.TextField('English',
+                               help_text='Natural translation of the text into English')
+    source = models.ForeignKey(Source, blank=True, null=True,
+                               help_text='Dictionary, book, speaker interview, etc. where this '
+                                         'word was found')
+    source_info = models.CharField('source details', max_length=200, default='', blank=True,
+                                   help_text='Page numbers, file IDs, dates, or other specific '
+                                             'information needed to find the word in the source.')
+    source_link = models.URLField(max_length=200, default='', blank=True,
+                                  help_text='A URL to link to the source. If not empty, this will '
+                                            'turn the "source details" into a hyperlink.')
+    varieties = models.ManyToManyField(Variety,
+                                       help_text='Broad language variety, dialect, or geographic '
+                                                 'region',
+                                       through='ExampleVarietyInfo',
+                                       blank=True)
+
+    def __unicode__(self):
+        return '{} | {}'.format(truncatechars(self.vernacular, 20),
+                                truncatechars(self.english, 20))
+
+
 class Entry(models.Model):
     entry = models.CharField('word', max_length=100)
     pos = models.CharField('part of speech',
-                           help_text='''To remove the part of speech entirely
-                           (show no ending tables for this word), enter "None".''',
+                           help_text='To remove the part of speech entirely '
+                                     '(show no ending tables for this word), enter "None".',
                            default='',
                            blank=True,
                            max_length=10)
     pos_auto = models.CharField('part of speech (guess)',
                                 editable=False,
-                                help_text='''Automatically detected based on the
-                                word and other information. If "part of speech" is
-                                blank, this will be used.''',
+                                help_text='Automatically detected based on the '
+                                          'word and other information. If "part of speech" is '
+                                          'blank, this will be used.',
                                 default='None',
                                 max_length=10)
     pos_final = models.CharField('Part of speech',
@@ -36,9 +83,9 @@ class Entry(models.Model):
     root = models.CharField(max_length=100, default='', blank=True)
     root_auto = models.CharField('root (guess)',
                                  editable=False,
-                                 help_text='''Automatically detected based on the
-                                 word and other information. If "root" is
-                                 blank, this will be used.''',  # TODO: document syntax
+                                 help_text='Automatically detected based on the '
+                                           'word and other information. If "root" is '
+                                           'blank, this will be used.',  # TODO: document syntax
                                  default='',
                                  max_length=100)
     root_final = models.CharField('Root',
@@ -47,9 +94,29 @@ class Entry(models.Model):
                                   max_length=100)
     defn = models.TextField('definition')
     search_text = models.TextField(editable=False, default='')
-    source = models.ForeignKey(Source, null=True)
-    source_info = models.CharField(max_length=200, default='', blank=True)
-    source_link = models.URLField(max_length=200, default='', blank=True)
+    source = models.ForeignKey(Source, blank=True, null=True,
+                               help_text='Dictionary, book, speaker interview, etc. where this '
+                                         'word was found')
+    source_info = models.CharField('source details', max_length=200, default='', blank=True,
+                                   help_text='Page numbers, file IDs, dates, or other specific '
+                                             'information needed to find the word in the source.')
+    source_link = models.URLField(max_length=200, default='', blank=True,
+                                  help_text='A URL to link to the source. If not empty, this will '
+                                            'turn the "source details" into a hyperlink.')
+    main_entry = models.ForeignKey('Entry', blank=True, null=True,
+                                   related_name='subentries',
+                                   help_text='If this is a sub-entry, link to the parent entry '
+                                             '(form without a postbase, for example).')
+    varieties = models.ManyToManyField(Variety, through='EntryVarietyInfo', blank=True,
+                                       help_text='Broad language variety, dialect, or geographic '
+                                                 'region')
+    examples = models.ManyToManyField(Example, through='EntryExampleInfo', blank=True)
+    see_also = models.ManyToManyField('Entry', through='SeeAlso', blank=True,
+                                      related_name='see_also_linked',
+                                      help_text='Other entries that might be useful to look at')
+
+    class Meta:
+        verbose_name_plural = "entries"
 
     def pos_or_auto(self):
         return self.pos or self.pos_auto
@@ -70,3 +137,50 @@ class Entry(models.Model):
 
     def __unicode__(self):
         return self.entry
+
+
+class EntryVarietyInfo(models.Model):
+    entry = models.ForeignKey(Entry)
+    variety = models.ForeignKey(Variety)
+    detail = models.CharField(max_length=200, default='', blank=True,
+                              help_text='Additional language variety details, e.g. city names, '
+                                        'Northern/Southern for Kodiak, or specific speakers')
+
+    def __unicode__(self):
+        return '{} ({})'.format(self.variety.abbrev, self.detail)
+
+
+class ExampleVarietyInfo(models.Model):
+    example = models.ForeignKey(Example)
+    variety = models.ForeignKey(Variety)
+    detail = models.CharField('details', max_length=200, default='', blank=True,
+                              help_text='Additional language variety details, e.g. city names, '
+                                        'Northern/Southern for Kodiak, or specific speakers')
+
+    def __unicode__(self):
+        return '{} ({})'.format(self.variety.abbrev, self.detail)
+
+
+class EntryExampleInfo(models.Model):
+    entry = models.ForeignKey(Entry)
+    example = models.ForeignKey(Example, verbose_name='example ID',
+                                help_text='Use the magnifying glass icon to search for or create '
+                                          'new examples, then select an example and its ID will '
+                                          'appear here.')
+
+    def __unicode__(self):
+        return 'Example for "{}": {}'.format(self.entry, truncatechars(self.example.vernacular, 20))
+
+
+class SeeAlso(models.Model):
+    source = models.ForeignKey(Entry,
+                               related_name='see_also_outgoing',
+                               help_text='The entry on which this "see also" will appear.')
+    target = models.ForeignKey(Entry,
+                               related_name='see_also_incoming',
+                               help_text='The entry this "see also" link will point to. Use the '
+                                         'magnifying glass icon to search for entires, then select '
+                                         'an entry and its ID will appear here.')
+
+    def __unicode__(self):
+        return '{}: see also "{}"'.format(self.source, self.target)
